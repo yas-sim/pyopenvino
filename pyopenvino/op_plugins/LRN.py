3985b3f2eb959efb9f20868989f0053e06f586ae
Yasunori Shimura <yasu0710@gmail.com>
2022-03-05 18:22:13 +0900

    Implemented an optimized LRN with Numpy

--- pyopenvino/op_plugins/LRN.py
+++ pyopenvino/op_plugins/LRN.py
@@ -23,6 +23,21 @@ def kernel_LRN_naive(inputs:dict, alpha:float, beta:float, bias:float, size:floa
     return res
 
 
+def kernel_LRN_numpy(inputs:dict, alpha:float, beta:float, bias:float, size:float):
+    input0 = inputs[0]
+    n,c,h,w = input0.shape
+
+    res = np.zeros_like(input0)
+
+    input_sq = input0 ** 2
+    denominator = np.zeros_like(input0)
+    for ch in range(c):
+        denominator[:,ch,:,:] = (bias+alpha*np.sum(input_sq[:, max(0,ch-size//2):min(c, ch+size//2+1), :, :], axis=1)) ** beta
+
+    res = input0 / denominator
+    return res
+
+
 def compute(node:dict, inputs:dict=None, kernel_type:str='naive', debug:bool=False):
     if debug:
         print(node)
@@ -38,8 +53,10 @@ def compute(node:dict, inputs:dict=None, kernel_type:str='naive', debug:bool=Fal
     bias  = float(node['data']['bias'])
     size  = int(node['data']['size'])
 
-    #if kernel_type == 'naive':
-    res = kernel_LRN_naive(inputs, alpha, beta, bias, size)
+    if kernel_type == 'naive':
+        res = kernel_LRN_naive(inputs, alpha, beta, bias, size)
+    else:
+        res = kernel_LRN_numpy(inputs, alpha, beta, bias, size)
 
     output_port_id = next(iter(node['output']))     # Get output port number
     res = { output_port_id:res }
